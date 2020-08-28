e20a27e91db138e6a911e5ebe2b023811e4b102a
HopVuVan <hopvv@nexlesoft.com>
2020-08-28 21:20:27 +0700

    update feedback => deploy

--- js/boxDropdown.js
+++ js/boxDropdown.js
@@ -2,6 +2,11 @@ function initBoxDropdown() {
   const items = document.querySelectorAll(".box-dropdown[datatype='boxDropdown']");
   items.forEach(box => {
     const buttonToggle = box.getElementsByClassName("box-dropdown__toggle-button")[0]
+
+    if (!buttonToggle) console.error("Can not init Box Dropdown cause by no any toggle-button")
+
+    const itemClickable = buttonToggle.parentElement;
+
     const contents = box.querySelectorAll(".box-dropdown-content");
     const shortContents = box.querySelectorAll(".box-dropdown-short-content");
     const contentDividers = box.querySelectorAll(".box-dropdown-content-divider");
@@ -13,7 +18,7 @@ function initBoxDropdown() {
         contentDivider && contentDivider.classList.contains("hidden") && contentDivider.classList.remove("hidden")
       })
       shortContents.forEach(shortContent => {
-        shortContent && shortContent.classList.contains("short-tow-line") && shortContent.classList.remove("short-tow-line")
+        shortContent && shortContent.classList.contains("short-one-line-three-dot") && shortContent.classList.remove("short-one-line-three-dot")
       })
     }
 
@@ -25,20 +30,22 @@ function initBoxDropdown() {
         contentDivider && !contentDivider.classList.contains("hidden") && contentDivider.classList.add("hidden")
       })
       shortContents.forEach(shortContent => {
-        shortContent && !shortContent.classList.contains("short-tow-line") && shortContent.classList.add("short-tow-line")
+        shortContent && !shortContent.classList.contains("short-one-line-three-dot") && shortContent.classList.add("short-one-line-three-dot")
       })
     }
 
-    buttonToggle.addEventListener('click', function (e) {
+    const itemEventClick = itemClickable.classList.contains("item-action") ? itemClickable : buttonToggle;
+
+    itemEventClick.addEventListener('click', function (e) {
       let classRotate = "rotate-180";
-      if (this.dataset.rotate === "rotate90") {
+      if (buttonToggle.dataset.rotate === "rotate90") {
         classRotate = "rotate-90";
       }
-      if (!this.classList.contains(classRotate)) {
-        this.classList.add(classRotate)
+      if (!buttonToggle.classList.contains(classRotate)) {
+        buttonToggle.classList.add(classRotate)
         showContent();
       } else {
-        this.classList.remove(classRotate)
+        buttonToggle.classList.remove(classRotate)
         hideContent();
       }
     })
